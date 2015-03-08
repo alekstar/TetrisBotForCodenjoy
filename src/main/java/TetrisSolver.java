c4c3cb044cfdb91e1847495df6758b2fc5dce21e
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-03-08 12:09:53 +0200

    realized algorithm to work with 'O' and 'I' figures

--- src/main/java/TetrisSolver.java
+++ src/main/java/TetrisSolver.java
@@ -4,9 +4,89 @@ public class TetrisSolver {
     final static int ROTATE_180_CLOCKWISE = 2;
     final static int ROTATE_90_COUNTERCLOCKWISE = 3;
 
+    private String moveLeft(int times) {
+        if (times < 0) {
+            throw new IllegalArgumentException("Argument times is less zero.");
+        }
+        return "left=" + times;
+    }
+
+    private String moveRight(int times) {
+        if (times < 0) {
+            throw new IllegalArgumentException("Argument times is less zero.");
+        }
+        return "right=" + times;
+    }
+
+    private String rotate(int times) {
+        if (times < 0) {
+            throw new IllegalArgumentException("Argument times is less zero.");
+        }
+        return "rotate=" + times;
+    }
+
+    private String drop() {
+        return "drop";
+    }
+
+    private TetrisCoordinates defineFreeCellCoordinatesForCube(Field field) {
+        for (int y = 0; y < field.defineDefaultFieldHeight(); y++) {
+            for (int x = 0; x < field.defineDefaultFieldWidth() - 1; x++) {
+                if (!field.isFilledCellAt(x, y)
+                        && !field.isFilledCellAt(x + 1, y)) {
+                    return TetrisCoordinates.create(x, y);
+                }
+            }
+        }
+        throw new RuntimeException("Can't find free cell");
+    }
+
+    private String defineCommandForCube(Field field) {
+        TetrisCoordinates coordinates = defineFreeCellCoordinatesForCube(field);
+        if (coordinates.getX() < 5) {
+            return moveLeft(4 - coordinates.getX()) + ", " + drop();
+        } else /* if (coordinates.getX() >= 5) */{
+            return moveRight(coordinates.getX() - 4) + ", " + drop();
+        }
+    }
+
+    private TetrisCoordinates defineFreeCellCoordinatesForLine(Field field) {
+        for (int y = 0; y < field.defineDefaultFieldHeight(); y++) {
+            for (int x = 0; x < field.defineDefaultFieldWidth(); x++) {
+                if (!field.isFilledCellAt(x, y)) {
+                    return TetrisCoordinates.create(x, y);
+                }
+            }
+        }
+        throw new RuntimeException("Can't find free cell");
+    }
+
+    private String defineCommandForLine(Field field) {
+        TetrisCoordinates coordinates = defineFreeCellCoordinatesForLine(field);
+        if (coordinates.getX() < 5) {
+            return moveLeft(4 - coordinates.getX()) + ", " + drop();
+        } else {
+            return moveRight(coordinates.getX() - 4) + ", " + drop();
+        }
+    }
+
+    private String defineCommand(Field field, String figure) {
+        if (figure.contentEquals("O")) {
+            return defineCommandForCube(field);
+        } else if (figure.contentEquals("I")) {
+            return defineCommandForLine(field);
+        } else {
+            return drop();
+        }
+    }
+
     public String answer(String figure, int x, int y, String glass, String next) {
         // add "drop" to response when you need to drop a figure
         // for details please check http://codenjoy.com/portal/?p=170#commands
-        return "left=4, right=3, rotate=0, drop";
+        // return "left=4, right=3, rotate=0, drop";
+
+        Field field = Field.create(glass);
+        return defineCommand(field, figure);
+        // return "left=4, drop";
     }
 }

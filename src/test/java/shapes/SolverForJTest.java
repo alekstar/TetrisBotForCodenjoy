5964d7c0614fae275773bd05baaf3822dc0a0f1c
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-04-15 12:09:55 +0300

    refactored shouldPutFigureAtRightCorner test case at SolverForJTest class - extracted field generation; - removed temporary variable expectedCommand; - removed temporary variable solver;

--- src/test/java/shapes/SolverForJTest.java
+++ src/test/java/shapes/SolverForJTest.java
@@ -11,14 +11,17 @@ public class SolverForJTest {
 
     @Test
     public void shouldPutFigureAtRightCorner() {
+        Field field =
+                defineFieldWithFilledBottomLineExceptTwoCellsInTheLeftCorner();
+        assertEquals("right=5, drop", SolverForJ.create(field)
+                .defineCommandLine());
+    }
+
+    private Field defineFieldWithFilledBottomLineExceptTwoCellsInTheLeftCorner() {
         Field field = Field.createStandartEmptyField();
         for (int x = 0; x < field.getWidth() - 2; x++) {
             field.fillCellAt(x, 0);
         }
-        FieldPrinter fieldPrinter = FieldPrinter.create(field);
-        fieldPrinter.print();
-        SolverForJ solver = SolverForJ.create(field);
-        String expectedCommand = "right=5, drop";
-        assertEquals(expectedCommand, solver.defineCommandLine());
+        return field;
     }
 }
